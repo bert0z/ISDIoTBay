4ce42a449480d4d2166c7861f495d6d0f52b99ce
bert0z <62048076+bert0z@users.noreply.github.com>
2020-06-08 16:35:42 +1000

    Update PAYMENT.sql

--- web/db/PAYMENT.sql
+++ web/db/PAYMENT.sql
@@ -4,7 +4,23 @@
  * and open the template in the editor.
  */
 /**
- * Author:  bert_
- * Created: 08/06/2020
+ * Author:  Jingwen CAO
+ * Created: 14/05/2020
  */
+drop table PAYMENT;
+create table PAYMENT
+(
+	ID INT not null primary key GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+	METHOD VARCHAR(100) not null,
+	AMOUNT DOUBLE default 0 not null,
+	CARDNAME VARCHAR(100),
+	CARDNUMBER VARCHAR(16),
+	EXPIRYDATE VARCHAR(4),
+	CVV VARCHAR(3),
+	PAYMENTDATE TIMESTAMP default CURRENT_TIMESTAMP,
+	ORDERID INTEGER
+);
 
+insert into PAYMENT(METHOD, AMOUNT, CARDNAME, CARDNUMBER, EXPIRYDATE, CVV, ORDERID)
+values ('Credit Card', 99.8, 'Molly', '1234567', '0622', '456', 1),
+('Direct Deposit', 99.8, 'Jack', '76543', '0622', '234', 1);
