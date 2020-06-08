726aa36f6119794888df373141916ae7e1517ec0
bert0z <62048076+bert0z@users.noreply.github.com>
2020-06-08 16:47:28 +1000

    merge fixes

--- src/java/controller/UpdatePayment.java
+++ src/java/controller/UpdatePayment.java
@@ -7,42 +7,30 @@ package controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.Payment;
+import model.dao.DBConnector;
+import model.dao.PaymentDAO;
 
 /**
  *
- * @author bert_
+ * @author caojingwen
  */
 public class UpdatePayment extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet UpdatePayment</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet UpdatePayment at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+    private DBConnector connector;
+    private PaymentDAO paymentDAO;
+    public UpdatePayment() throws ClassNotFoundException, SQLException {
+        connector = new DBConnector();
+        paymentDAO = new PaymentDAO(connector.openConnection());
     }
+
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
@@ -56,7 +44,14 @@ public class UpdatePayment extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        String pid= request.getParameter("pid");
+        try {
+            Payment p  = paymentDAO.searchPayments(pid, null, null).get(0);
+            request.setAttribute("payment", p);
+            request.getRequestDispatcher("updatePayment.jsp").forward(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(DeletePayment.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -70,7 +65,21 @@ public class UpdatePayment extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        String pid = request.getParameter("pid");
+        String method = request.getParameter("method");
+        String orderId = request.getParameter("orderId");
+        String amount = request.getParameter("amount");
+        String cardName = request.getParameter("cardName");
+        String cardNumber = request.getParameter("cardNumber");
+        String expiryDate = request.getParameter("expiryDate");
+        String cvv = request.getParameter("cvv");
+        
+        try {
+            paymentDAO.updatePayment(pid, method, orderId, amount, cardName, cardNumber, expiryDate, cvv);
+            response.sendRedirect("MyPayment");
+        } catch (SQLException ex) {
+            Logger.getLogger(AddPaymentServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
