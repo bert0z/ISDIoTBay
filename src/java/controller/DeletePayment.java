ee486e6744c71b5ec9d588904ad858478e20a295
bert0z <62048076+bert0z@users.noreply.github.com>
2020-06-08 16:45:00 +1000

    Merge Errors

--- src/java/controller/DeletePayment.java
+++ src/java/controller/DeletePayment.java
@@ -7,41 +7,37 @@ package controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.dao.DBConnector;
+import model.dao.PaymentDAO;
 
 /**
  *
- * @author bert_
+ * @author caojingwen
  */
 public class DeletePayment extends HttpServlet {
 
+    
+    private DBConnector connector;
+    private PaymentDAO paymentDAO;
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
+     * Handles the HTTP <code>GET</code> method.
      *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet DeletePayment</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet DeletePayment at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+    public DeletePayment() throws ClassNotFoundException, SQLException {
+        connector = new DBConnector();
+        paymentDAO = new PaymentDAO(connector.openConnection());
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -56,7 +52,13 @@ public class DeletePayment extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        String pid= request.getParameter("pid");
+        try {
+            paymentDAO.deletePayment(pid);
+            response.sendRedirect("MyPayment");
+        } catch (SQLException ex) {
+            Logger.getLogger(DeletePayment.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -70,7 +72,6 @@ public class DeletePayment extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
     }
 
     /**
